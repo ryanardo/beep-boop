6040a88be56a8d835e4b687009e5233e1c0bcf51
Ryan G Ferris <ryanglenferris@gmail.com>
2017-12-08 10:31:25 -0800

    add backend logic for hasZero function

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,16 @@ var countUp =  function(num) {
   console.log("countUp: numArray["+numArray+"]");
 }; // End of 'countUp' function.
 
+// SPEC 2: Can determine if a number has a 0 in it.
+var hasZero = function(num) {
+  if (num.toString().match(zero)) {
+    console.log(num+" contains 0 value in it.");
+    return true;
+  } else {
+    console.log(num+" does not contain a 0 value.");
+  }
+}; // End of 'hasZero' function.
+
 // FRONT END LOGIC
 $(document).ready(function() {
   $("#form1").submit(function(event) {
@@ -27,5 +37,7 @@ $(document).ready(function() {
 
     //countUp
     var countUp1 = countUp(num);
+    //hasZero
+    var hasZero1 = hasZero(num);
   }); //End of 'form submit' listener.
 }); // End of 'document.ready' listener.
