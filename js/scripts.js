dd6329e4beef59ba030eb2b61b4b125b8415ee5e
Ryan G Ferris <ryanglenferris@gmail.com>
2017-12-08 13:15:46 -0800

    add backend logic to script1 branch to test

--- js/scripts.js
+++ js/scripts.js
@@ -6,17 +6,59 @@ var beepBoop = function(number) {
 var num;
 var numString;
 var numReplaced;
-var numArray = [];
+var numbers = [];
+var numbersArchive = [];
+var newArray = [];
+var otherArray = [];
 var zero = 0;
 var one = 1;
+
 // SPEC 1: It can count up to the provided number.
 var countUp =  function(num) {
+  numbers.length = 0;
   for(i=0; i <= num; i++) {
-    numArray.push(i)
+    numbers.push(i);
+    numbersArchive.push(i);
   };
-  console.log("countUp: numArray["+numArray+"]");
-  return false;
+  console.log("countUp: numbers["+numbers+"]");
+  return numbers;
 }; // End of 'countUp' function.
+// SPEC 6: It can replace a value with another value.
+var replaceValue = function(num) {
+  if (hasZero(num) === true) {
+    var numReplaced = "'Beep!'"
+    console.log("replace "+num+" with "+numReplaced);
+    otherArray.push(numReplaced);
+    return numReplaced;
+  } else if (hasOne(num) === true) {
+    var numReplaced = "'Boop!'"
+    console.log("replace "+num+" with "+numReplaced);
+    otherArray.push(numReplaced);
+    return numReplaced;
+  } else if (divisibleByThree(num) === true) {
+    var numReplaced = "I'm sorry, Dave. I'm afraid I can't do that."
+    console.log("replace "+num+" with '"+numReplaced+"'");
+    otherArray.push(numReplaced);
+    return numReplaced;
+  } else {
+    console.log(num+" does not contain either the digits 0 or 1, and is not divisible by 3.");
+    otherArray.push(numReplaced);
+    return num;
+  }
+}; // End of 'replaceValue' function.
+
+
+// SPEC 7: START
+// SPEC 7: Create an array of numbers
+
+
+
+
+// SPEC 7: END
+
+//////////////////////////////////////////////////////////
+// START: COMPLETED SPECS
+//////////////////////////////////////////////////////////
 // SPEC 2: Can determine if a number has a 0 in it.
 var hasZero = function(num) {
   if (num.toString().match(zero)) {
@@ -56,33 +98,15 @@ var isNumber =  function(num) {
     console.log(num+" is a number.");
   }
 }
-// SPEC 6: It can replace a value with another value.
-var replaceValue = function(num) {
-  if (hasZero(num) === true) {
-    var numReplaced = "'Beep!'"
-    console.log("replace "+num+" with "+numReplaced);
-    return numReplaced;
-  } else if (hasOne(num) === true) {
-    var numReplaced = "'Boop!'"
-    console.log("replace "+num+" with "+numReplaced);
-    return numReplaced;
-  } else if (divisibleByThree(num) === true) {
-    var numReplaced = "I'm sorry, Dave. I'm afraid I can't do that."
-    console.log("replace "+num+" with '"+numReplaced+"'");
-    return numReplaced;
-  } else {
-    console.log(num+" does not contain either the digits 0 or 1, and is not divisible by 3.");
-    return num;
-  }
-}; // End of 'replaceValue' function.
-
+//////////////////////////////////////////////////////////
+// END: COMPLETED SPECS
+//////////////////////////////////////////////////////////
 // FRONT END LOGIC
 $(document).ready(function() {
   $("#form1").submit(function(event) {
     event.preventDefault();
     var num = parseInt($("#input1").val());
     var numString = $("#input1").val();
-    var numArray = [];
     var results = beepBoop(num);
     $("#output1").text(results);
 
@@ -98,10 +122,15 @@ $(document).ready(function() {
     var isNumberTest = isNumber(num);
     //TEST: replaceValue
     var replaceValueTest = replaceValue(num);
+    //TEST: testArray0
+    var testArray0 = numbers.map(function(num) {
+      $("#output1").append("<li>" + replaceValue(num) + "</li>");
+        return replaceValue(num);
+        console.log("testArray0["+testArray2+"]");
+
+    });
 
 /* TEST FUNCTION TEMPLATES
-    //TEST: function_name
-    var function_name Test = function_name(num);
     //TEST: function_name
     var function_name Test = function_name(num);
     //TEST: function_name
