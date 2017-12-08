a572c5555367f41dc16b70901de8d1b2f0525708
Ryan G Ferris <ryanglenferris@gmail.com>
2017-12-08 10:47:15 -0800

    add backend logic for divisibleByThree function

--- js/scripts.js
+++ js/scripts.js
@@ -14,27 +14,38 @@ var countUp =  function(num) {
     numArray.push(i)
   };
   console.log("countUp: numArray["+numArray+"]");
+  return false;
 }; // End of 'countUp' function.
-
 // SPEC 2: Can determine if a number has a 0 in it.
 var hasZero = function(num) {
   if (num.toString().match(zero)) {
     console.log(num+" contains 0 value in it.");
     return true;
   } else {
-    console.log(num+" does not contain a 0 value.");
+    console.log(num+" does NOT contain a 0 value.");
+    return false;
   }
 }; // End of 'hasZero' function.
-
 // SPEC 3: Can determine if a number has a 1 in it.
 var hasOne = function(num) {
   if (num.toString().match(one)) {
     console.log(num+" contains 1 value in it.");
     return true;
   } else {
-    console.log(num+" does not contain a 1 value.");
+    console.log(num+" does NOT contain a 1 value.");
+    return false;
   }
 }; //End of 'hasOne' function.
+// SPEC 4: Program can divide a number by 3.
+var divisibleByThree = function(num) {
+  if (num % 3 === 0) {
+    console.log(num+" is divisible by 3");
+    return true;
+  } else {
+    console.log(num+" is NOT divisible by 3");
+    return false;
+  }
+}; // End of 'divisibleByThree' function.
 
 // FRONT END LOGIC
 $(document).ready(function() {
@@ -46,11 +57,34 @@ $(document).ready(function() {
     $("#output1").text(results);
 
     //TEST: countUp
-    var countUp1 = countUp(num);
+    var countUp_Test = countUp(num);
     //TEST: hasZero
-    var hasZero1 = hasZero(num);
+    var hasZero_Test = hasZero(num);
     //TEST: hasOne
-    var hasOne1 = hasOne(num);
-    //TEST:
+    var hasOne_Test = hasOne(num);
+    //TEST: divisibleByThree
+    var divisibleByThree_Test = divisibleByThree(num);
+
+/* TEST FUNCTION TEMPLATES
+    //TEST: function_name
+    var function_name Test = function_name(num);
+    //TEST: function_name
+    var function_name Test = function_name(num);
+    //TEST: function_name
+    var function_name Test = function_name(num);
+    //TEST: function_name
+    var function_name Test = function_name(num);
+    //TEST: function_name
+    var function_name Test = function_name(num);
+    //TEST: function_name
+    var function_name Test = function_name(num);
+    //TEST: function_name
+    var function_name Test = function_name(num);
+    //TEST: function_name
+    var function_name Test = function_name(num);
+    //TEST: function_name
+    var function_name Test = function_name(num);
+*/
+
   }); //End of 'form submit' listener.
 }); // End of 'document.ready' listener.
