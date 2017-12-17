e07a6e2db806f313ccb55a70bcc5f1c876dc3be6
Ryan G Ferris <ryanglenferris@gmail.com>
2017-12-16 19:09:48 -0800

    separate back/front end logic
    correct separation of logic found in the 'js/scripts .js' file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,70 +1,46 @@
-// BACK END LOGIC
+/**BACK END LOGIC**/
 var num;
 var numArray = [];
 var convertArray = [];
 var beep = "Beep!";
 var boop = "Boop!";
 var dave = "I'm sorry, Dave. I'm afraid I can't do that.";
+// Create array of numbers starting at 0.
 var numbers = function(num) {
-  for(i = 0; i <= num; i++) {
-    if (i % 3 == 0 && i.toString().includes("1") && i.toString().includes("0")) {
-      $("#output1").append("<li class='list-group-item'>"+i+"</li>");
-      $("#output2").append("<li class='list-group-item'>"+beep+" "+boop+" "+dave);
-      numArray.push(i);
-      convertArray.push(beep+" "+boop+" "+dave);
-      console.log(i+" All Criteria met!");
-    } else if (i % 3 === 0 && i.toString().includes("1")) {
-      $("#output1").append("<li class='list-group-item'>"+i+"</li>");
-      $("#output2").append("<li class='list-group-item'>"+beep+" "+dave+"</li>");
-      numArray.push(i);
-      convertArray.push(beep+" "+dave);
-      console.log(i+" contains a One AND is divisible by 3.");
-    } else if (i % 3 === 0 && i.toString().includes("0")) {
-      $("#output1").append("<li class='list-group-item'>"+i+"</li>");
-      $("#output2").append("<li class='list-group-item'>"+boop+" "+dave+"</li>");
-      numArray.push(i);
-      convertArray.push(boop+" "+dave);
-      console.log(i+" contains a One AND is divisible by 3.");
-    } else if ((i.toString().includes("0")) && (i.toString().includes("1"))) {
-      $("#output1").append("<li class='list-group-item'>"+i+"</li>");
-      $("#output2").append("<li class='list-group-item'>"+beep+" "+boop+"</li>");
-      numArray.push(i);
-      convertArray.push(beep+" "+boop);
-      console.log(i+" contained a Zero AND a One.");
-    } else if (i.toString().includes("0")) {
-      $("#output1").append("<li class='list-group-item'>"+i+"</li>");
-      $("#output2").append("<li class='list-group-item'>"+beep+"</li>");
-      numArray.push(i);
+  for (i = 0; i <= num; i++) {
+    numArray.push(i);
+    if (i.toString().includes("0")) {
       convertArray.push(beep);
-      console.log(i+" contained a Zero value.");
     } else if (i.toString().includes("1")) {
-      $("#output1").append("<li class='list-group-item'>"+i+"</li>");
-      $("#output2").append("<li class='list-group-item'>"+boop+"</li>");
-      numArray.push(i);
       convertArray.push(boop);
-      console.log(i+" contained a One value.");
     } else if (i % 3 === 0) {
-      $("#output1").append("<li class='list-group-item'>"+i+"</li>");
-      $("#output2").append("<li class='list-group-item'>"+dave+"</li>");
-      numArray.push(i);
       convertArray.push(dave);
-      console.log(i+" was divisible by Three.");
     } else {
-      $("#output1").append("<li class='list-group-item'>"+i+"</li>");
-      $("#output2").append("<li class='list-group-item'>"+i+"</li>");
-      numArray.push(i);
       convertArray.push(i);
-      console.log(i+" met NONE of the Criteria.");
     }
   }; // End of the 'for-loop'.
 }; // End of 'numbers' function.
-
-// FRONT END LOGIC
+/**FRONT END LOGIC**/
 $(document).ready(function() {
   $("#form1").submit(function(event) {
     event.preventDefault();
-    $(".list-group-item").remove();
     var num = parseInt($("#input1").val());
-    var results = numbers(num);
+    $(".list-group-item").remove();
+    // Push 'numArray' items to list
+    var numArrayList = function(numArray) {
+      for(i = 0; i < numArray.length; i++) {
+        $("#output1").append("<li class='list-group-item'>"+numArray[i]+"</li>");
+      }; // End of the 'numArrayList' for-loop.
+    }; // End of 'numArrayList' function
+    // Push 'convertArray' items to list.
+    var convertArrayList = function(convertArray) {
+      for(i = 0; i < convertArray.length; i++) {
+        $("#output2").append("<li class='list-group-item'>"+convertArray[i]+"</li>");
+      }; // End of the 'convertArrayList' for-loop.
+    }; // End of the 'convertArrayList' function.
+    // Call functions to display data.
+    numbers(num);
+    numArrayList(numArray);
+    convertArrayList(convertArray);
   }); //End of 'form submit' listener.
 }); // End of 'document.ready' listener.
